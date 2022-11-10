361f46d2fd51b5741a130a7f889610bd206508a2
Shubham Rathi <shra9449@colorado.edu>
2022-11-10 16:43:19 -0700

    Added parallel i/o code

--- hpsc_lab10/codes/transientDiffusion_1mat_ForLab10/src/dump.h
+++ hpsc_lab10/codes/transientDiffusion_1mat_ForLab10/src/dump.h
@@ -9,6 +9,9 @@ void write_mpiio_dump(mpiInfo &myMPI)
 
   printf("************* Writing dump file *****************\n");
 
+  //rLOOP
+    //printf("phi: %f", phi[r]);
+
 
   // Using the hpsc2022/codes/mpiio_01/main.cpp sample code from class lecture as a 
   // guide, write phi to a single output file from any number of PEs using the
@@ -26,6 +29,48 @@ void write_mpiio_dump(mpiInfo &myMPI)
   // demonstrate that it is being written correctly to the binary output file.
   //
   // 
+
+  MPI_Datatype myPartOfGlobal;
+  
+
+  
+
+  
+  
+  int idxStartThisPE  [1] = { 1  };  // Index coordinates of the sub-array inside this PE's array, A
+  int AsizeThisPE     [1] = { nField + 2 };  // Size of the A array on this PE    
+  int sub_AsizeThisPE [1] = { nField };  // Size of the A-sub-array on this PE
+
+  // (5.2) Ask MPI to create that new datatype
+  //
+  // ------------------------------------------------------------------------------------------------------
+  //
+  // Number of elements in A in each dimension                   Starting indices of the subarray inside
+  //                          of the sub array                   the this PE's A array
+  //                                         |                   |
+  //  Number of elements in each             |                   |           Order of the A array in memory, row
+  // dimension of this PE's array            |                   |           major or column major
+  //                            |            |                   |           |
+  //    Number of dimensions    |            |                   |           |
+  //                       |    |            |                   |           |     
+  //                       |    |            |                   |           |     
+  MPI_Type_create_subarray(1, AsizeThisPE, sub_AsizeThisPE, idxStartThisPE, MPI_ORDER_C, MPI_DOUBLE, &myPartOfGlobal);
+
+  MPI_Type_commit(&myPartOfGlobal);
+  
+
+  MPI_File fh;
+  MPI_File_open(MPI_COMM_WORLD, "phi_dump.bin", MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &fh);
+
+  MPI_File_set_view (fh, 0, MPI_DOUBLE, myPartOfGlobal, "native", MPI_INFO_NULL);
+
+  MPI_File_write_all(fh, &phi[1], 1, myPartOfGlobal, MPI_STATUS_IGNORE);
+
+  MPI_File_close(&fh);
+
+  MPI_Type_free(&myPartOfGlobal);
+  //MPI_Finalize();
+  return;
   
 }
 
